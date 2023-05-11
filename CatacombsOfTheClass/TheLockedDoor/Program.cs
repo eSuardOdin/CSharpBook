ab0b0d174cfa42aaa528f902f1ed9f78c2686a37
eSuardOdin <erwannsuard@gmail.com>
2023-05-11 20:08:41 +0200

    Door program almost finished

--- CatacombsOfTheClass/TheLockedDoor/Program.cs
+++ CatacombsOfTheClass/TheLockedDoor/Program.cs
@@ -1,13 +1,54 @@
 ﻿
 Door door = new Door();
+bool running = true;
+while (running == true)
+{
+    string action = "";
+    Console.WriteLine($"What do you want to do with the door ? (now {door.Status})");
+    Console.WriteLine("\t- change : Change the passcode");
+    Console.WriteLine("\t- open : Open the door");
+    Console.WriteLine("\t- close : Close the door");
+    Console.WriteLine("\t- lock : Lock the door");
+    Console.WriteLine("\t- unlock : Unlock the door");
+    Console.WriteLine("\t- quit : Quit program");
+    Console.Write("Action : ");
+    action = Console.ReadLine();
 
-door.ChangePassCode();
+    switch (action)
+    {
+        case "change":
+            door.ChangePassCode();
+            break;
+        case "open":
+            if(door.Open()) Console.WriteLine("Door " + door.Status);
+            else Console.WriteLine("Door must be unlocked (closed)");
+            break;
+        case "close":
+            if(door.Close()) Console.WriteLine("Door " + door.Status);
+            else Console.WriteLine("Door must be open to be closed");
+            break;
+        case "lock":
+            if(door.Lock()) Console.WriteLine("Door " + door.Status);
+            else Console.WriteLine("Door must be closed");
+            break;
+        case "unlock":
+            if(door.Unlock(Helper.AskPasscode("Enter the passcode -> "))) Console.WriteLine("Door " + door.Status);
+            else Console.WriteLine("Error in unlocking");
+            break;
+        case "quit":
+            running = false;
+            break;
+        default:
+            Console.WriteLine("Wrong input !");
+            break;
+    }
+
+}
 
 
 public class Door {
     private string _passcode;
-    // public string Passcode {get;}
-    public Status Status {get;}
+    public Status Status {get; private set;}
 
 
     public Door(){
@@ -38,6 +79,35 @@ public class Door {
                 break;
             }
         }
+    }
+
+    public bool Close() {
+        if(Status == Status.Open) {
+            Status = Status.Closed;
+            return true;
+        }
+        return false;
+    }
+    public bool Open() {
+        if(Status == Status.Closed) {
+            Status = Status.Open;
+            return true;
+        }
+        return false;
+    }
+    public bool Lock() {
+        if(Status == Status.Closed) {
+            Status = Status.Locked;
+            return true;
+        }
+        return false;
+    }
+    public bool Unlock(string pass) {
+        if(Status == Status.Locked && pass == _passcode) {
+            Status = Status.Closed;
+            return true;
+        }
+        return false;
     }
 }
 
