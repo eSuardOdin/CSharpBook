dcf103617f743ee0fa2722eb78bfa70b585a6d6e
unknown <erwannsuard@gmail.com>
2023-05-11 14:54:31 +0200

    Did the card exercise

--- CatacombsOfTheClass/TheCard/Program.cs
+++ CatacombsOfTheClass/TheCard/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Create a list of cards and add every card in it
+List<Card> Deck = new List<Card>();
+foreach(Color color in Enum.GetValues(typeof(Color))){
+    foreach(Rank rank in Enum.GetValues(typeof(Rank))) {
+        Card card = new Card(color, rank);
+        Deck.Add(card);
+    }
+} 
+
+// Print cards 
+foreach (Card card in Deck)
+{
+    card.GetCardDescription();
+}
+
+
+class Card
+{
+    public Color Color {get;}
+    public Rank Rank {get;}
+    public Card(Color color, Rank rank) {
+        Color = color;
+        Rank = rank;
+    }
+
+    public void GetCardDescription() {
+        Console.WriteLine($"- The {Color} {Rank}");
+    }
+}
+
+enum Color {Red, Green, Blue, Yellow}
+enum Rank  {One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, DollarSign, Percent, Caret, Ampersand}
