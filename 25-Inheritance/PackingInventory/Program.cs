a307da6f4875f021f6238dfa15b20f9968da75e2
eSuardOdin <esuardodin@gmail.com>
2023-05-15 20:16:12 +0200

    Working on inheritance

---
+++ 25-Inheritance/PackingInventory/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Create a Pack class to manage items
+Each pack has three limits: The total number of items it can hold, the weight it can
+carry and the volume it can hold. Each item has a weight and volume.
+
+*/
+Bow bow = new Bow();
+System.Console.WriteLine(bow.Volume);
+
+
+
+
+
+
+
+public class Pack {
+    public int MaxItems {get;}
+    public float MaxWeight {get;}
+    public float MaxVolume {get;}
+}
+
+
+
+
+public class InventoryItem {
+    public float Weight {get; protected set;}
+    public float Volume {get; protected set;}
+
+    public InventoryItem(float weight, float volume) {
+        Weight = weight;
+        Volume = volume;
+    }
+}
+
+
+public class Arrow : InventoryItem {
+    public Arrow() : base(0.1f, 0.05f) {}
+}
+
+
+public class Bow : InventoryItem {
+    public Bow() : base(1f, 4f) {}
+}
