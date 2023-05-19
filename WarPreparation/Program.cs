e571dce3e82eb80dd8af14f31c6dadcc2e4c9d0d
eSuardOdin <esuardodin@gmail.com>
2023-05-19 07:29:47 +0200

    discovered records

---
+++ WarPreparation/Program.cs
@@ -0,0 +1,23 @@
+﻿Sword original = new Sword(Material.iron, Gemstones.empty, 12.0f, 3.5f);
+Console.WriteLine(original);
+Sword second = original with {material = Material.binarium, gemstone = Gemstones.sapphire};
+Console.WriteLine(second);
+
+public record Sword(Material material, Gemstones gemstone, float length, float width);
+
+public enum Material {
+    wood,
+    bronze,
+    iron,
+    steel,
+    binarium
+}
+
+public enum Gemstones {
+    emerald,
+    amber,
+    sapphire,
+    diamond,
+    bitsone,
+    empty
+}
