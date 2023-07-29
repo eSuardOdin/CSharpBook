9aa04d00f5bfee451bcac9afa0dc86ed4261c130
eSuardOdin <esuardodin@gmail.com>
2023-07-29 20:20:03 +0200

    Structs added

---
+++ secondAttempt/structs/PositionStruct/Program.cs
@@ -0,0 +1,18 @@
+﻿Coordinate a = new Coordinate(0, 0);
+Coordinate b = new Coordinate(5, -1);
+
+Console.WriteLine($"{a.IsAdjacent(b)}");
+public struct Coordinate
+{
+    public readonly int X {get;}
+    public readonly int Y {get;}
+    public Coordinate(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    public bool IsAdjacent(Coordinate other) => 
+        this.X == other.X + 1 || this.Y == other.Y + 1 || 
+        this.X == other.X - 1 || this.Y == other.Y - 1; 
+}
