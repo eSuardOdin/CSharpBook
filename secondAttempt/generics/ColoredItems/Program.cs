bfcbf8314d4200ac5e7eaaa7c9f7111538410e8e
eSuardOdin <esuardodin@gmail.com>
2023-07-30 16:17:54 +0200

    Done generics exercise

---
+++ secondAttempt/generics/ColoredItems/Program.cs
@@ -0,0 +1,31 @@
+﻿var bow = new Bow();
+var axe = new Axe();
+var sword = new Sword();
+
+var blueSword = new ColoredItem<Sword>(sword, ConsoleColor.DarkBlue);
+var redBow = new ColoredItem<Bow>(bow, ConsoleColor.Red);
+var greenAxe = new ColoredItem<Axe>(axe, ConsoleColor.Green);
+var invisibleSword = new ColoredItem<Sword>(sword, ConsoleColor.White);
+public class Bow {}
+public class Sword {}
+public class Axe {}
+
+public class ColoredItem<T> 
+{
+    public T Item { get; set;}
+    public ConsoleColor Color{ get; set;}
+
+
+    public ColoredItem(T item, ConsoleColor color)
+    {
+        Item = item;
+        Color = color;
+        Display();
+    }
+
+    private void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.WriteLine($"This {Item.GetType()} is {Color}");
+    }
+}
