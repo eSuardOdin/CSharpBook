a035f2d81383ed83ea563d5b7086daf7f77bab30
eSuardOdin <esuardodin@gmail.com>
2023-08-03 07:24:59 +0200

    Played with events like a n00b

--- secondAttempt/events/Tests/Program.cs
+++ secondAttempt/events/Tests/Program.cs
@@ -1,6 +1,6 @@
 ﻿Ship ship = new Ship(3);
 SoundEffectManager sem = new(ship);
-
+var exploded = false;
 while(ship.Health > 0)
 {
     Console.WriteLine($"Ship's life : {ship.Health}");
@@ -8,6 +8,11 @@ while(ship.Health > 0)
     Console.ReadLine();
     ship.TakeDamage(1);
     Console.WriteLine("");
+    if(ship.Health == 0 && !exploded) 
+    { 
+        ship.Regenerate(); 
+        exploded = !exploded;    
+    }
 }
 
 public class Ship
@@ -25,25 +30,60 @@ public class Ship
         Health -= amount;
         if (Health <= 0) 
         {
-            ShipExploded(DateTime.Now);
+            ShipExploded?.Invoke(DateTime.Now); // Check if null event (if no handlers attached)
         }
     }
+
+    public void Regenerate() => Health = 3; // Just for testing purpose
 }
 
 
 
 public class SoundEffectManager 
 {
+    private Ship ShipObj { get; set;}
 
     // We attach the method to the event in the constructor
     public SoundEffectManager(Ship ship)
     {
-        ship.ShipExploded += OnShipExploded;
+        ShipObj = ship;
+        ShipObj.ShipExploded += OnShipExploded;
     }
-    private void OnShipExploded(DateTime now) => PlaySound($"Boooooooooooom badaboum biiiiiing, ship exploded on {now}");
+    private void OnShipExploded(DateTime now)
+    {
+        PlaySound($"Boooooooooooom badaboum biiiiiing, ship exploded on {now}");
+        // Cleanup after the first explosion, no event anymore
+        Cleanup();
+    }
 
-    public void PlaySound(string message)
+    private void PlaySound(string message)
     {
         Console.WriteLine(message);
-    } 
+    }
+
+    // To clean the event and let the garbage collector clean the object
+    private void Cleanup()
+    {
+        ShipObj.ShipExploded -= OnShipExploded;
+    }
 }
+
+
+// public class FrenchSoundManager
+// {
+//     public FrenchSoundManager(Ship ship)
+//     {
+//         ship.ShipExploded += HandleExplosion;
+//     }
+
+//     private void HandleExplosion(DateTime now) 
+//     {
+//         Console.WriteLine($"C'est bien dommage, votre vaisseau a explosé le {now}");
+//     }
+
+//     // To clean the event and let the garbage collector clean the object
+//     private void Cleanup()
+//     {
+//         Ship.ShipExploded -= HandleExplosion;
+//     }
+// }
