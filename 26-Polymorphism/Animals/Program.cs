cb0f8eca505c8fabfcb7ed52025d50eaf1461533
eSuardOdin <esuardodin@gmail.com>
2023-08-07 00:08:31 +0200

    Working on fs exercice

---
+++ 26-Polymorphism/Animals/Program.cs
@@ -0,0 +1,29 @@
+﻿var sheep = new Sheep();
+sheep.Battlecry();
+
+
+public abstract class Animal
+{
+    protected bool IsCarnivor { get; set; }
+    public string Name { get; set; }
+
+    public Animal(bool isCarnivor, string name)
+    {
+        IsCarnivor = isCarnivor;
+        Name = name;
+    }
+
+    public abstract void Battlecry();
+
+    protected string Food() => IsCarnivor ? "Flesh" : "Leaf";
+} 
+
+
+public class Sheep : Animal
+{
+    public Sheep() : base(false, "Sheep") {}
+    public override void Battlecry() 
+    {
+        Console.WriteLine($"I'm a sheep and I eat {this.Food()}");
+    }
+}
--- secondAttempt/files/TheLongGame/Program.cs
+++ secondAttempt/files/TheLongGame/Program.cs
@@ -8,7 +8,7 @@
 */
 
 
-Console.Write("Please enter your name : ");
+/* Console.Write("Please enter your name : ");
 var name = Console.ReadLine();
 while (string.IsNullOrEmpty(name));
 {
@@ -16,4 +16,58 @@ while (string.IsNullOrEmpty(name));
     name = Console.ReadLine();
 } 
 
-if(!Directory.Exists(""))
+if(!File.Exists("")) { File.Create()} */
+
+Game game = new("./Scores");
+
+
+public class Player
+{
+    public string Name { get; private set; }
+    public int Score { get; private set; }
+
+    public Player(string name, int score)
+    {
+        Name = name;
+        Score = score;
+    }
+}
+
+public class Game
+{
+    private List<Player> Players { get; set; }
+    private Player CurrentPlayer { get; set;}
+    private string ScorePath { get; set; }
+    public Game(string path)
+    {
+        Players = new List<Player>();
+        ScorePath = path;
+        if(!Directory.Exists(ScorePath)) { Directory.CreateDirectory(ScorePath); }
+        foreach(var file in Directory.EnumerateFiles(ScorePath))
+        {
+            // Get player name
+            string name = Path.GetFileNameWithoutExtension(file);
+            // Get Player score, if no score, delete file.
+            int score;
+            if (Int32.TryParse(File.ReadAllText(file), out score))
+            {
+                Player player = new Player(name, score);
+                Players.Add(player);
+                Console.WriteLine($"Player : {player.Name}");
+                Console.WriteLine($"Score : {player.Score}");
+            }
+            else
+            {
+                Console.WriteLine($"File '{file}' not in a valid format, deleting it");
+                File.Delete(file);
+            }          
+        }
+
+        private Player GetPlayer(string name)
+        {
+            Players.FirstOrDefault
+        }
+    }
+
+
+}
