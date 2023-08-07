31cd8f26a2b6a10918703331d9c0bda709d13444
eSuardOdin <esuardodin@gmail.com>
2023-08-07 08:51:30 +0200

    Can update score in a file. Added a SaveManager class

--- secondAttempt/files/TheLongGame/Program.cs
+++ secondAttempt/files/TheLongGame/Program.cs
@@ -19,12 +19,20 @@ while (string.IsNullOrEmpty(name));
 if(!File.Exists("")) { File.Create()} */
 
 Game game = new("./Scores");
-
+SaveManager sm = new("./Scores");
+Player player = sm.LoadPlayer("valid");
+Console.WriteLine($"{player.Name} {player.Score}");
+Console.Write("Entre un score pour le test : ");
+int test;
+Int32.TryParse(Console.ReadLine(), out test);
+player.Score = test;
+sm.SavePlayer(player);
+Console.WriteLine($"UPDATE : {player.Name} {player.Score}");
 
 public class Player
 {
     public string Name { get; private set; }
-    public int Score { get; private set; }
+    public int Score { get; /*private*/ set; }
 
     public Player(string name, int score)
     {
@@ -63,11 +71,53 @@ public class Game
             }          
         }
 
-        private Player GetPlayer(string name)
+    }
+}
+
+
+public class SaveManager
+{
+    private string SavePath { get; set; }
+    public SaveManager(string path)
+    {
+        SavePath = path;
+        int score;
+        // Delete invalid files in folder
+        foreach(var fileName in Directory.EnumerateFiles(SavePath))
         {
-            Players.FirstOrDefault
+           if (!Int32.TryParse(File.ReadAllText(fileName), out score)) { File.Delete(fileName); }
         }
     }
 
+    public bool PlayerExists(string name)
+    {
+        foreach(var file in Directory.EnumerateFiles(SavePath))
+        {
+            if(Path.GetFileNameWithoutExtension(file) == name) { return true; }
 
+        }
+        return false;
+    }
+
+    public Player LoadPlayer(string name)
+    {
+        foreach(var file in Directory.EnumerateFiles(SavePath))
+        {
+            if(Path.GetFileNameWithoutExtension(file) == name) 
+            {
+                int score;
+                if (Int32.TryParse(File.ReadAllText(file), out score))
+                {
+                    Player player = new Player(name, score);
+                    return player;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void SavePlayer(Player player)
+    {
+        File.WriteAllText($"{SavePath}/{player.Name}.txt", player.Score.ToString());
+    }
 }
