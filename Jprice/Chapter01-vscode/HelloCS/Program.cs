c0027bcdfc82a58b61c6b02f86eb32e75a44f929
eSuardOdin <erwannsuard@gmail.com>
2023-08-13 11:08:11 +0200

    still working on new book

--- Jprice/Chapter01-vscode/HelloCS/Program.cs
+++ Jprice/Chapter01-vscode/HelloCS/Program.cs
@@ -1,4 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Arguments
+    // WriteLine($"There are {args.Length} arguments");
+// foreach (var arg in args)
+// {
+//     WriteLine(arg);
+// }
 
-int z;
+
+// if(args.Length < 3)
+// {
+//     WriteLine("You must specify two colors and a cursor size");
+//     return;
+// }
+
+// ForegroundColor = (ConsoleColor)Enum.Parse(
+//     enumType: typeof(ConsoleColor),
+//     value: args[0],
+//     ignoreCase: true); // -> Case insensitive
+
+
+// BackgroundColor = (ConsoleColor)Enum.Parse(
+//     enumType: typeof(ConsoleColor),
+//     value: args[1],
+//     ignoreCase: true);
+
+// try
+// {
+//     CursorSize = int.Parse(args[2]); // -> Only supported on windows
+// }
+// catch (PlatformNotSupportedException)
+// {
+//     WriteLine("Not supported on this platform");
+// }
+#endregion
+
+#region ReadKey
+// Write("Press any key combination: ");
+// ConsoleKeyInfo key = ReadKey();
+// WriteLine();
+// WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+//     arg0: key.Key,
+//     arg1: key.KeyChar,
+//     arg2: key.Modifiers); 
+#endregion
+
+#region Async
+// HttpClient client = new();
+// HttpResponseMessage res = 
+//     await client.GetAsync("http://apple.com/");
+// Console.WriteLine($"Apple home page has {res.Content.Headers.ContentLength} bytes");
+#endregion
