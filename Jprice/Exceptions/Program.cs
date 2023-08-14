b5bd784cc8206c9cdf823f7bbc9641ac0ad6b82e
eSuardOdin <erwannsuard@gmail.com>
2023-08-14 05:26:03 +0200

    Exceptions in Jprice

---
+++ Jprice/Exceptions/Program.cs
@@ -0,0 +1,23 @@
+﻿// PAGE 169
+
+Console.WriteLine("Before parsing");
+Console.Write("How old are you: ");
+string? input = Console.ReadLine();
+
+try
+{
+    int age = int.Parse(input);
+    Console.WriteLine($"You are {age} years old");
+}
+
+catch(FormatException)  // If you know the exception type
+{
+    Console.WriteLine("The input you entered is an invalid age format");
+}
+catch (Exception ex)   // If there is another (here, an overflow for instance)
+{
+    Console.WriteLine($"{ex.GetType} says \"{ex.Message}\"");
+}
+
+
+Console.WriteLine("After parsing");
