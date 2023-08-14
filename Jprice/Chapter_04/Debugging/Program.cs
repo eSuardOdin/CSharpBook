7f3acaeeb3dfb3f31dc251afd7bd280db950d735
eSuardOdin <erwannsuard@gmail.com>
2023-08-14 06:34:50 +0200

    Debug and breakpoints

---
+++ Jprice/Chapter_04/Debugging/Program.cs
@@ -0,0 +1,12 @@
+﻿double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+
+Console.WriteLine($"{a} + {b} = {answer}");
+Console.WriteLine("Press enter to end the app");
+Console.ReadLine();
+
+double Add(double a, double b)
+{
+    return a * b; // Not what it's supposed to do
+}
