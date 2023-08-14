563132380402db0e5012614a668c6f61aa0347cf
eSuardOdin <esuardodin@gmail.com>
2023-08-14 08:18:52 +0200

    Instrumenting, before adding NuGet Packages

---
+++ Jprice/Chapter_04/Instrumenting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+string logPath = "./log.txt";
+
+Console.WriteLine($"Writing to: {logPath}");
+
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+
+Trace.Listeners.Add(logFile);
+
+Trace.AutoFlush = true; // Does not wait to the buffer to be full before writing
+Debug.WriteLine("Debug says : Watching"); // dotnet run --configuration Debug
+Trace.WriteLine("Trace says : Watching"); // dotnet run --configuration Debug + Release
